bc360e75fa0169b9cf40dae5f9f597409bbd8049
kantho202 <kanthochakma2025@gmail.com>
2026-02-12 17:25:22 +0600

    register page update

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,44 +1,53 @@
-import React from 'react';
-import { FaUser } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash, FaUser } from 'react-icons/fa';
 import { LuCircleUser } from 'react-icons/lu';
-import { MdEmail, MdOutlineEmail } from 'react-icons/md';
+import { MdEmail } from 'react-icons/md';
 import { TbLockPassword } from 'react-icons/tb';
 import { Link } from 'react-router';
 import styled from 'styled-components';
-
-const Login = () => {
+const Register = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const handleLogin = (data) => {
+        console.log('after login', data)
+    }
+    const handleTogglePasswordShow = (e) => {
+        e.preventDefault()
+        setShowPassword(!showPassword)
+    }
     return (
         <div className='flex justify-center '>
             <StyledWrapper>
-                <form className="form">
-                    <div className="flex-column">
-                        <label>Full Name </label></div>
-                    <div className="inputForm">
-                        <FaUser></FaUser>
-                        <input type="text" className="input" placeholder="Enter your full name" />
-                    </div>
-                    <div className="flex-column">
-                        <label>Profile Photo </label></div>
-                    <div className="inputForm">
-                        <LuCircleUser size={20} />
-                        <input type="text" className="input" placeholder="Enter your photo url" />
-                    </div>
+                <form onSubmit={handleSubmit(handleLogin)} className="form">
+
                     <div className="flex-column">
                         <label>Email </label></div>
                     <div className="inputForm">
-                       <MdEmail size={20} />
-                        <input type="text" className="input" placeholder="Enter your Email" />
+                        <MdEmail></MdEmail>
+                        <input type="text" {...register('email')} className="input" placeholder="Enter your Email" />
+                        {
+                            errors.email?.type === "required" &&
+                            <p className='text-red-600'>Email is required</p>
+                        }
                     </div>
                     <div className="flex-column">
                         <label>Password </label></div>
                     <div className="inputForm">
                         <TbLockPassword size={20} />
-                        <input type="password" className="input" placeholder="Enter your Password" />
-                        <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" /></svg>
+                        <input type={showPassword ? 'text' : 'password'} {...register('password')}
+                            className="input" placeholder="Enter your Password" />
+                        <span className='cursor-pointer' onClick={handleTogglePasswordShow}>{showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}</span>
                     </div>
-
+                    <div className="flex-row">
+                        <div>
+                            <input type="checkbox" />
+                            <label>Remember me </label>
+                        </div>
+                        <span className="span">Forgot password?</span>
+                    </div>
                     <button className="button-submit">Sign In</button>
-                    <p className="p">Don't have an account? <Link to={'/register'} className="span">Sign Up</Link>
+                    <p className="p">Don't have an account? <Link to={'/register'} className="span">Sign in</Link>
                     </p><p className="p line">Or With</p>
                     <div className="flex-row">
                         <button className="btn google">
@@ -182,5 +191,4 @@ const StyledWrapper = styled.div`
     border: 1px solid #2d79f3;
     ;
   }`;
-
-export default Login;
+export default Register;
